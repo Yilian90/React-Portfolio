3ca0a13bab42f01090fb0482356baa4e8e62a975
Yilian90 <chenyilian90@gmail.com>
2023-11-21 12:50:15 +0800

    create navigation bar to be display

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import styles from './App.module.css'
+import { Navbar } from "./components/Navbar/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className={styles.App}>Hello World!</div>
+    <div className={styles.App}>
+      <Navbar />
+    </div>
   )
 }
 
--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+import styles from './Navbar.module.css';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+  <nav className={styles.navbar}>
+    <a className={styles.title} href="/">
+      Portfolio
+    </a>
+    <div className={styles.menu}>
+      <ul className={styles.menuItems}>
+        <li>
+          <a href="#about">About</a>
+          </li>
+        <li>
+          <a href="#experience">Experience</a>
+          </li>
+        <li>
+          <a href="#projects">Projects</a>
+          </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+};
